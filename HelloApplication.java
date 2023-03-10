a2489ebddbaacb9387738d0428661118ad6cf200
TupoaMudille <71610105+TupoaMudille@users.noreply.github.com>
2023-03-10 12:18:34 +0300

    Update HelloApplication.java
    все еще без относительных путей

--- HelloApplication.java
+++ HelloApplication.java
@@ -2,188 +2,209 @@ package com.example.shooting;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.util.ArrayList;
+
 import java.io.IOException;
 
-import javafx.animation.Timeline;
-
-public class HelloApplication extends Application {
-
+public class HelloApplication extends Application
+{
     private static final double W = 600, H = 400;
     private static final String HERO_IMAGE_LOC = "U:/Пронина Татьяна/Documents/hero_image.png";
     private static final String HERO_WEAPON_LOC = "U:/Пронина Татьяна/Documents/weapon_image.png";
     private static final String TARGET_IMAGE_LOC = "U:/Пронина Татьяна/Documents/target_image.png";
     private static final String SUPER_TARGET_IMAGE_LOC = "U:/Пронина Татьяна/Documents/super_target_image.png";
-    Text gameStop = new Text(W/2-50,H/2,"Game paused");
-    private Timeline timeline = new Timeline();
-    private Image heroImage, weaponImage, targetImage;
-    private Node hero;
-    private ArrayList<Node> weapons = new ArrayList<Node>();
-    private ArrayList<Node> targets = new ArrayList<Node>();
     private Group board;
     Text scoreText, shootText;
-    private int dTarget, dWeapon = 10, shootCount = 0, score = 0;
-    boolean goNorth, goSouth, throwing;
+    private int shootCount = 0, score = 0;
+    private Image heroImage=new Image(HERO_IMAGE_LOC), weaponImage=new Image(HERO_WEAPON_LOC), targetImage1 = new Image(TARGET_IMAGE_LOC),targetImage2 = new Image(SUPER_TARGET_IMAGE_LOC);
+    private ImageView hero = new ImageView(heroImage), target1=new ImageView(targetImage1),target2=new ImageView(targetImage2), aWeapon = new ImageView(weaponImage);
+    private TargetThread targetThread1 = new TargetThread();
+    private TargetThread targetThread2 = new TargetThread();
+    boolean isD1=false,isD2=false;
 
-    private boolean running = true, isUp=false;
+
+    private void TargetInit()
+    {
+        targetThread1.SetTarget(target1, 500, 200, 10);
+        targetThread2.SetTarget(target2, 300, 200, 20);
+    }
+
+    private void CheckHit()
+    {
+        if (aWeapon.getBoundsInParent().intersects(target1.getBoundsInParent())&&!isD1)
+        {
+            score+=2;
+            isD1=true;
+        }
+        if (aWeapon.getBoundsInParent().intersects(target2.getBoundsInParent())&&!isD2)
+        {
+            score++;
+            isD2=true;
+        }
+        scoreText.setText("Score: " + score);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
-        heroImage = new Image(HERO_IMAGE_LOC);
-        hero = new ImageView(heroImage);
+            scoreText = new Text(110, 10, "Score: " + score);
+            shootText = new Text(170, 10, "Shoots: " + shootCount);
 
-        weaponImage = new Image(HERO_WEAPON_LOC);
+            board = new Group();
 
-        board = new Group();
-        scoreText = new Text(110, 10, "Score: " + score);
-        shootText = new Text(170, 10, "Shoots: " + shootCount);
-        board.getChildren().addAll(hero, scoreText, shootText);
-        moveHeroTo(0, H / 2);
-        Scene scene = new Scene(board, W, H, Color.GREENYELLOW);
-        createTarget();
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                switch (keyEvent.getCode()) {
-                    case W:
-                        goNorth = true;
-                        break;
-                    case S:
-                        goSouth = true;
-                        break;
-                    case SPACE:
-                        if (!throwing) {
+            board.getChildren().addAll(hero, target1, target2, scoreText, shootText, aWeapon);
+            Scene scene = new Scene(board, W, H, Color.YELLOWGREEN);
+            hero.relocate(0, H / 2);
+            aWeapon.relocate(10, H / 2);
+            stage.setTitle("Shooter Game");
+            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    switch (keyEvent.getCode()) {
+                        case SPACE:
                             shootCount++;
-                            shootText.setText("Shoots: "+shootCount);
-                            ImageView aWeapon = new ImageView(weaponImage);
-                            Node newWeapon = aWeapon;
-                            newWeapon.relocate(hero.getLayoutX() + hero.getBoundsInLocal().getWidth(), hero.getLayoutY());
-                            weapons.add(newWeapon);
-                            board.getChildren().add(newWeapon);
-                            throwing = true;
-                        }
-                        break;
-                    case ESCAPE:
-                        stopGame();
-                        break;
-                    case TAB:
-                        startGame();
-                        break;
-
+                            shootText.setText("Shoots: " + shootCount);
+                            PlayerThread playerThread = new PlayerThread();
+                            playerThread.SetPlayer(aWeapon);
+                            if (!playerThread.isAlive()) {
+                                playerThread.start();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            }
-        });
-
-        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                switch (keyEvent.getCode()) {
-                    case W:
-                        goNorth = false;
-                        break;
-                    case S:
-                        goSouth = false;
-                        break;
-                    case SPACE:
-                        throwing = false;
-                        break;
+            });
+            AnimationTimer timer = new AnimationTimer() {
+                @Override
+                public void handle(long l) {
+                    CheckHit();
                 }
-            }
-        });
-        stage.setTitle("Shooter Game");
-        stage.setScene(scene);
-        stage.show();
-        AnimationTimer timer = new AnimationTimer() {
-            public void handle(long now) {
-                if (!running)
-                    return;
-                int dy = 0;
-                if (goNorth) dy -= 3;
-                if (goSouth) dy += 3;
-                moveHeroTo(hero.getLayoutX(),hero.getLayoutY()+dy);
-                throwWeapon(dWeapon);
-                moveTarget();
-                checkHit();
-            }
-        };
-        timer.start();
+            };
+
+            timer.start();
+            stage.setScene(scene);
+            TargetInit();
+            targetThread1.start();
+            targetThread2.start();
+            stage.show();
+        }
+        
+    @Override
+    public void stop() throws Exception
+    {
+        targetThread2.stop();
+        targetThread1.stop();
+        super.stop();
+    }
+    public static void main(String[] args)
+    {
+        launch();
     }
 
-    private void moveHeroTo(double x,double y){
-        if(y>=0 && y<=H-hero.getBoundsInLocal().getHeight()) hero.relocate(x,y);
-    }
-
-    private void moveTarget() {
-        for (int i = 0; i < targets.size(); i++) {
-            if (targets.get(i).getLayoutY() == 0) {
-            isUp = false;
-        } else if (targets.get(i).getLayoutY() == H) isUp = true;
-            targets.get(i).relocate(targets.get(i).getLayoutX(), targets.get(i).getLayoutY() + (isUp ?-dTarget : dTarget));
+    private class MovTarget implements Runnable
+    {
+        private ImageView target;
+        private double newY;
+        private double newX;
+        public MovTarget(ImageView _target, double _newX, double _newY)
+        {
+            this.target = _target;
+            this.newY=_newY;
+            this.newX = _newX;
+        }
+        @Override
+        public void run()
+        {
+            target.relocate(this.newX, this.newY);
         }
     }
 
-    void createTarget() {
-        String loc;
-        if (Math.random() < 0.5) {
-            loc = TARGET_IMAGE_LOC;
-            dTarget = 8;
-        } else {
-            loc = SUPER_TARGET_IMAGE_LOC;
-            dTarget = 4;}
-            targetImage = new Image(loc);
-            Node newTarget = new ImageView(targetImage);
-            newTarget.relocate(W / 2, H / 2);
-            targets.add(newTarget);
-            board.getChildren().add(newTarget);
+    private class PlayerThread extends Thread
+    {
+        private ImageView weapon;
+        private double weaponW;
+        private double weaponH;
+        public void SetPlayer(ImageView _weapon)
+        {
 
-    }
+            this.weapon=_weapon;
+            this.weaponW=this.weapon.getLayoutX();
+            this.weaponH=this.weapon.getLayoutY();
+        }
 
-    private void throwWeapon(int del){
-        for(int i=0;i<weapons.size();i++)
-            if(weapons.get(i).getLayoutX()<W)
-                weapons.get(i).relocate(weapons.get(i).getLayoutX()+del,weapons.get(i).getLayoutY());
-            else weapons.remove(i);
-    }
-
-    private void checkHit(){
-        for(int i=0;i<weapons.size();i++)
-            for(int j=0;j<targets.size();j++)
-                if(weapons.get(i).getBoundsInParent().intersects(targets.get(j).getBoundsInParent())){
-                        board.getChildren().remove(targets.get(j));
-                        targets.remove(j);
-                        score++;
-                        board.getChildren().remove(weapons.get(i));
-                        weapons.remove(i);
-                        createTarget();
-                        scoreText.setText("Score: "+score);
+        @Override
+        public void run()
+        {
+            double pos = this.weaponW;
+            while (true)
+            {
+                pos++;
+                Platform.runLater(new MovTarget(this.weapon, pos, this.weaponH));
+                try
+                {
+                    Thread.sleep(1);
                 }
+                catch (Exception exception)
+                {
+                    return;
+                }
+                if(pos>=W)
+                {
+                    Platform.runLater(new MovTarget(this.weapon, 10, H/2));
+                    isD1=false;
+                    isD2=false;
+                    return;
+                }
+            }
+        }
     }
 
-    void startGame(){
-        board.getChildren().remove(gameStop);
-        timeline.play();
-        running=true;
-    }
-
-    void stopGame(){
-        running=false;
-        gameStop.setFill(Color.GREEN);
-        gameStop.setFont(Font.font("Verdana",20));
-        board.getChildren().add(gameStop);
-        timeline.stop();
-    }
-    public static void main(String[] args) {
-        launch();
+    private class TargetThread extends Thread
+    {
+        private ImageView target;
+        private int speed;
+        private double startH;
+        private double startW;
+        public void SetTarget(ImageView _target, double _w, double _h, int _speed)
+        {
+            this.target=_target;
+            this.target.relocate(_w, _h);
+            this.speed=_speed;
+            this.startH=_h;
+            this.startW=_w;
+        }
+        @Override
+        public void run()
+        {
+            double pos = startH;
+            int vec = 1;
+            while (true)
+            {
+                try
+                {
+                    Thread.sleep(speed);
+                    Thread.yield();
+                }
+                catch (Exception exception)
+                {
+                    return;
+                }
+                pos+=vec;
+                Platform.runLater(new MovTarget(this.target, startW, pos));
+                if(pos>H || pos<0)
+                {
+                    vec*=(-1);
+                }
+            }
+        }
     }
 }
